31d3f2d1c13b7dcee6efdc057cfa7379ec6c2608
D.Kirichik <d.kirichik@mail.ru>
2016-06-03 00:15:37 +0300

    something works

--- lab5/lab5.c
+++ lab5/lab5.c
@@ -36,7 +36,7 @@ void set_projection(){
     glLoadIdentity();
 }
 
-int is_inside_polygon(struct point*, struct point);
+int is_inside_polygon(struct point*, struct point, size_t);
 void Satherlend_Hodgman(struct point*, struct point*);
 
 static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods){
@@ -50,7 +50,7 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
         clip = 1;
         int i;
         for(i = 0; i < clipper_count; i++){
-            if(is_inside_polygon(polygon_vertices, clipper_vertices[i])){
+            if(is_inside_polygon(polygon_vertices, clipper_vertices[i], polygon_count)){
                 clipped_polygon_vertices[clipped_polygon_count] = clipper_vertices[i];
                 clipped_polygon_count++;
             }
@@ -98,12 +98,12 @@ struct point* intersection(struct point first_start, struct point first_end, str
     second_vector[1] = second_end.y - second_start.y;
     float vec_prod_first = first_vector[0] * (second_start.y - first_start.y) - first_vector[1] * (second_start.x - first_start.x);
     float vec_prod_second = first_vector[0] * (second_end.y - first_start.y) - first_vector[1] * (second_end.x - first_start.x);
-    //printf("vec_proc1 = %f %f\n", vec_prod_first, vec_prod_second);
+    printf("vec_proc1 = %f %f\n", vec_prod_first, vec_prod_second);
     if(sign(vec_prod_first) == sign(vec_prod_second) || (vec_prod_first == 0) || vec_prod_second == 0)
         return NULL;
     vec_prod_first = second_vector[0] * (first_start.y - second_start.y) - second_vector[1] * (first_start.x - second_start.x);
     vec_prod_second = second_vector[0] * (first_end.y - second_start.y) - second_vector[1] * (first_end.x - second_start.x);
-    //printf("vec_proc2 = %f %f\n", vec_prod_first, vec_prod_second);
+    printf("vec_proc2 = %f %f\n", vec_prod_first, vec_prod_second);
     if(sign(vec_prod_first) == sign(vec_prod_second) || vec_prod_first == 0 || vec_prod_second == 0)
         return NULL;
     struct point* intersect = (struct point*) malloc(sizeof(struct point));
@@ -112,10 +112,10 @@ struct point* intersection(struct point first_start, struct point first_end, str
     return intersect;         
 }
 
-int is_inside_polygon(struct point* pl, struct point p){
+int is_inside_polygon(struct point* pl, struct point p, size_t length){
     int c = 0;
     int i, j;    
-    for (i = 0, j = polygon_count-1; i < polygon_count; j = i++) {
+    for (i = 0, j = length-1; i < length; j = i++) {
         if ((((pl[i].y<=p.y) && (p.y<pl[j].y)) || ((pl[j].y<=p.y) && (p.y<pl[i].y))) && (p.x > (pl[j].x - pl[i].x) * (p.y - pl[i].y) / (pl[j].y - pl[i].y) + pl[i].x)){
             printf("%d\n", c);
             c = !c;
@@ -125,34 +125,44 @@ int is_inside_polygon(struct point* pl, struct point p){
     return c;
 }
 
+struct point fp;
+
+int cmpfunc (const void *a, const void *b){
+    //TODO polat plot compare
+    struct point* f = (struct point*) a;
+    struct point* s = (struct point*) b;
+    
+    float length1 = (f->x - fp.x)*(f->x - fp.x) + (f->y - fp.y)*(f->y-fp.y);
+    float length2 = (s->x - fp.x)*(s->x - fp.x) + (s->y - fp.y)*(s->y-fp.y);
+    return length1>length2;
+}
+
 void Satherlend_Hodgman(struct point* polygon_vertices, struct point* clipper_vertices){
     int i, j;
     for(i = 0; i < polygon_count; i++){ 
-        int intersection_flag = 0;
+        int start = clipped_polygon_count;
         struct point* intersect = NULL;
         for(j = 0; j < clipper_count; j++){
             intersect = intersection(polygon_vertices[i], polygon_vertices[(i+1) % polygon_count], clipper_vertices[j], clipper_vertices[(j+1) % clipper_count]); 
             printf("intersect %d%d with %d%d = %p\n", j, (j+1) % clipper_count, i, (i+1) % polygon_count, intersect);
             if(intersect != NULL){
-                intersection_flag = 1;
                 clipped_polygon_vertices[clipped_polygon_count] = *intersect;
                 clipped_polygon_count++;
                 free(intersect);
             }
         }
-        if(intersection_flag == 0){
-            if(!is_inside_polygon(clipper_vertices, polygon_vertices[i])){
+        if(is_inside_polygon(clipper_vertices, polygon_vertices[i], clipper_count)){
                 printf("!");
                 clipped_polygon_vertices[clipped_polygon_count] = polygon_vertices[i];
                 clipped_polygon_count++;
-                if(i+1 == polygon_count)
-                    return;
-                clipped_polygon_vertices[clipped_polygon_count] = polygon_vertices[(i+1) % polygon_count];
-                clipped_polygon_count++;
-                i++;
+                //clipped_polygon_vertices[clipped_polygon_count] = polygon_vertices[(i+1) % polygon_count];
+                //clipped_polygon_count++;
             }
+        fp = polygon_vertices[i];
+        qsort(clipped_polygon_vertices + start, clipped_polygon_count - start, sizeof(struct point), cmpfunc);
+            
         }
-    }
+    return;
 }
 
 void draw_clipper(){
@@ -166,7 +176,7 @@ void draw_clipper(){
 }
 
 void draw_polygon(){
-    glBegin(GL_POLYGON);
+    glBegin(GL_LINE_LOOP);
     int i;
     for(i = 0; i < polygon_count; i++){
         glColor3f(0.f,0.f,1.f);
@@ -175,13 +185,17 @@ void draw_polygon(){
     glEnd();
 }
 
+
+
 void draw_clipped_polygon(){
-    glBegin(GL_POLYGON);
+    //qsort(clipped_polygon_vertices,clipped_polygon_count, sizeof(struct point), cmpfunc);
+    //TODO reorder clockwise
+    glBegin(GL_LINE_LOOP);
     int i;
     for(i = 0; i < clipped_polygon_count; i++){
         glColor3f(1.f,0.f,0.f);
         glVertex2f(clipped_polygon_vertices[i].x , clipped_polygon_vertices[i].y);
-        printf("clipped_polygon %d = %f %f\n", i, clipped_polygon_vertices[i].x, clipped_polygon_vertices[i].y);
+        //printf("clipped_polygon %d = %f %f\n", i, clipped_polygon_vertices[i].x, clipped_polygon_vertices[i].y);
     }
     glEnd();
 }
@@ -209,12 +223,11 @@ GLFWwindow* init_window(){
 
 void draw(GLFWwindow *window){
     glClear(GL_COLOR_BUFFER_BIT);
-    glMatrixMode(GL_MODELVIEW);
+    draw_clipper();
     if(polygon)
         draw_polygon();
     if(clip)
         draw_clipped_polygon();
-    draw_clipper();
     glfwSwapBuffers(window);
     glfwPollEvents();
 }
