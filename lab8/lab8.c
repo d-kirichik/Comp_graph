657ca26546974ec3aeabe98d7638f89763e96997
D.Kirichik <d.kirichik@mail.ru>
2016-08-20 14:40:27 +0300

    :)

--- lab8/lab8.c
+++ lab8/lab8.c
@@ -1,23 +1,5 @@
-/******************************************************************************\
-| OpenGL 4 Example Code.                                                       |
-| Accompanies written series "Anton's OpenGL 4 Tutorials"                      |
-| Email: anton at antongerdelan dot net                                        |
-| First version 27 Jan 2014                                                    |
-| Copyright Dr Anton Gerdelan, Trinity College Dublin, Ireland.                |
-| See individual libraries for separate legal notices                          |
-|******************************************************************************|
-| "Hello Triangle". Just the basics.                                           |
-| If you're on Mac un-comment the version number code at the beginning. It     |
-| will give you the latest, even if you say 3.2!                               |
-| This uses the libraries GLEW and GLFW3 to start GL. Download and compile     |
-| these first. Linking them might be a pain, but you'll need to master this.   |
-|                                                                              |
-| I wrote this so that it compiles in pedantic ISO C90, to show that it's      |
-| easily done. I usually use minimalist C++ though, for tidier-looking maths   |
-| functions.                                                                   |
-\******************************************************************************/
-#include <GL/glew.h> /* include GLEW and new version of GL on Windows */
-#include <GLFW/glfw3.h> /* GLFW helper library */
+#include <GL/glew.h> 
+#include <GLFW/glfw3.h> 
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -113,61 +95,38 @@ void form_array_for_drawing(){
 }
 	
 int create_shaders () {
-	/* geometry to use. these are 3 xyz points (9 floats total) to make a
-     triangle */
-
-	/* these are the strings of code for the shaders
-	the vertex shader positions each vertex point */
 	const char* vertex_shader =
 	"#version 130\n"
 	"in vec3 vp;"
 	"void main () {"
 	"	gl_Position = vec4 (vp, 1.0);"
 	"}";
-	/* the fragment shader colours each fragment (pixel-sized area of the
-	triangle) */
 	const char* fragment_shader =
 	"#version 130\n"
 	"out vec4 frag_colour;"
 	"void main () {"
 	"	frag_colour = vec4 (0.5, 0.0, 0.5, 1.0);"
 	"}";
-	/* GL shader objects for vertex and fragment shader [components] */
 	GLuint vs, fs;
-	/* GL shader programme object [combined, to link] */
 	
-	/* start GL context and O/S window using the GLFW helper library */
 	if (!glfwInit ()) {
 		fprintf (stderr, "ERROR: could not start GLFW3\n");
 		return 1;
 	} 
 
-	/* tell GL to only draw onto a pixel if the shape is closer to the viewer
-     */
-	glEnable (GL_DEPTH_TEST); /* enable depth-testing */
-    /* with LESS depth-testing interprets a smaller value as "closer" */
+	glEnable (GL_DEPTH_TEST); 
 	glDepthFunc (GL_LESS);
-	/* a vertex buffer object (VBO) is created here. this stores an array of
-     data on the graphics adapter's memory. in our case - the vertex points */
 	glGenBuffers (1, &vbo);
 	glBindBuffer (GL_ARRAY_BUFFER, vbo);
 	glBufferData (GL_ARRAY_BUFFER, (scene.torus_param.partition + 1) * (scene.torus_param.partition + 1) * 6 * sizeof (GLfloat), drawable,
                   GL_STATIC_DRAW);
 	
-	/* the vertex array object (VAO) is a little descriptor that defines which
-	data from vertex buffer objects should be used as input variables to vertex
-	shaders. in our case - use our only VBO, and say 'every three floats is a 
-	variable' */
 	glGenVertexArrays (1, &vao);
 	glBindVertexArray (vao);
 	glEnableVertexAttribArray (0);
 	glBindBuffer (GL_ARRAY_BUFFER, vbo);
 	glVertexAttribPointer (0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
 	
-	/* here we copy the shader strings into GL shaders, and compile them. we
-     then create an executable shader 'program' and attach both of the compiled
-     shaders. we link this, which matches the outputs of the vertex shader to
-     the inputs of the fragment shader, etc. and it is then ready to use */
 	vs = glCreateShader (GL_VERTEX_SHADER);
 	glShaderSource (vs, 1, &vertex_shader, NULL);
 	glCompileShader (vs);
@@ -185,12 +144,8 @@ void draw(GLFWwindow* window){
 	glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glUseProgram (shader_programme);
 	glBindVertexArray (vao);
-		/* draw points 0-3 from the currently bound VAO with current in-use
-         shader */
 	glDrawArrays(GL_TRIANGLE_STRIP, 0, 2 * (scene.torus_param.partition + 1) * (scene.torus_param.partition + 1));
-		/* update other events like input handling */
     glfwPollEvents ();
-		/* put the stuff we've been drawing onto the display */
 	glfwSwapBuffers (window);
 }
 
